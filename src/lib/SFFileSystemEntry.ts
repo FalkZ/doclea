0635632e022e09b4f3c09a87fa18a97d8827e077
Falk Zwimpfer <zwimpfal@students.zhaw.ch>
2022-03-13 17:42:31 +0100

    new interface names

--- src/lib/SFFileSystemEntry.ts
+++ src/lib/SFFileSystemEntry.ts
@@ -2,35 +2,35 @@ import type { Result, OkOrError } from './utitities'
 import type { SFError } from './SFError'
 import type { SFFile } from './SFFile'
 
-export interface SFFileSystemProvider {
+export interface StorageFrameworkProvider {
   // todo: empty?
-  open(): Result<SFFileSystemEntry, SFError>
+  open(): Result<StorageFrameworkEntry, SFError>
 }
 
-export interface SFFileSystemDirectoryEntry extends SFFileSystemEntry {
-  readonly isDirectory: true
-  readonly isFile: false
-  getChildren(): Result<SFFileSystemFileEntry[], SFError>
-  createFile(name: string): Result<SFFileSystemFileEntry, SFError>
-  createDirectory(name: string): Result<SFFileSystemDirectoryEntry, SFError>
-}
-
-interface SFFileSystemEntry {
+interface StorageFrameworkEntry {
   // todo: mabye path class
   // todo: last modified
   readonly fullPath: string
   readonly isDirectory: boolean
   readonly isFile: boolean
   readonly name: string
-  getParent(): Result<SFFileSystemDirectoryEntry | null, SFError>
-  moveTo(directory: SFFileSystemDirectoryEntry): OkOrError<SFError>
+  getParent(): Result<StorageFrameworkDirectoryEntry | null, SFError>
+  moveTo(directory: StorageFrameworkDirectoryEntry): OkOrError<SFError>
   rename(name: string): OkOrError<SFError>
   remove(): OkOrError<SFError>
 }
 
-export interface SFFileSystemFileEntry extends SFFileSystemEntry {
+export interface StorageFrameworkFileEntry extends StorageFrameworkEntry {
   readonly isDirectory: false
   readonly isFile: true
   read(): Result<SFFile, SFError>
   save(file: File): OkOrError<SFError>
+}
+
+export interface StorageFrameworkDirectoryEntry extends StorageFrameworkEntry {
+  readonly isDirectory: true
+  readonly isFile: false
+  getChildren(): Result<StorageFrameworkEntry[], SFError>
+  createFile(name: string): Result<StorageFrameworkFileEntry, SFError>
+  createDirectory(name: string): Result<StorageFrameworkDirectoryEntry, SFError>
 }
