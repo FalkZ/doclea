de2420c0df87cd927f0b1489adfaa85cae7ba7eb
FalkZ <24669719+FalkZ@users.noreply.github.com>
2022-04-23 12:40:33 +0200

    added skeleton

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,34 @@
 module.exports = {
-  extends: ['ijw']
+  extends: ['ijw'],
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase']
+      },
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE']
+      },
+      {
+        selector: 'parameter',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow'
+      },
+      {
+        selector: 'memberLike',
+        modifiers: ['private'],
+        format: ['camelCase']
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase']
+      },
+      {
+        selector: 'enumMember',
+        format: ['PascalCase']
+      }
+    ]
+  }
 }
