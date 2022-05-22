0c6d52f4daf16544d052ac62388e3eb6f4a9588e
Marc Meyer <meyerm18@students.zhaw.ch>
2022-05-22 21:21:34 +0200

    Add pathUtil

--- storage-framework/src/local-fs-adapter/LocalFileSystem.ts
+++ storage-framework/src/local-fs-adapter/LocalFileSystem.ts
@@ -1,4 +1,4 @@
-import type { SFError } from '../lib/SFError'
+import { SFError } from '../lib/SFError'
 import type {
   StorageFrameworkEntry,
   StorageFrameworkProvider
@@ -8,20 +8,25 @@ import { ReactivityDirDecorator } from '../lib/wrappers/ReactivityDecorator'
 import { Result } from '../lib/utilities/result'
 import { LocalFallbackDirectoryEntry } from './local-fallback-fs-adapter/LocalFallbackDirectoryEntry'
 import { LocalDirectoryEntry } from './LocalDirectoryEntry'
+import { PathUtil } from '../lib/utilities/pathUtil'
 
 export class LocalFileSystem implements StorageFrameworkProvider {
   open(): Result<StorageFrameworkEntry, SFError> {
-    return new Result(async (resolve) => {
+    return new Result(async (resolve, reject) => {
       if (window.showDirectoryPicker) {
-        const dirHandle = await window.showDirectoryPicker({
-          multiple: true
-        })
-        resolve(
-          new ReactivityDirDecorator(
-            null,
-            new LocalDirectoryEntry(dirHandle, null, true)
+        try {
+          const dirHandle = await window.showDirectoryPicker({
+            multiple: true
+          })
+          resolve(
+            new ReactivityDirDecorator(
+              null,
+              new LocalDirectoryEntry(dirHandle, null, true)
+            )
           )
-        )
+        } catch (err) {
+          reject(new SFError('No directory provided', err))
+        }
       } else {
         const el = document.createElement('input')
         el.setAttribute('type', 'file')
@@ -30,18 +35,19 @@ export class LocalFileSystem implements StorageFrameworkProvider {
         el.click()
 
         el.onchange = (ev: any) => {
-          const dirName = ev.target?.files?.length
-            ? '/' + ev.target.files[0].webkitRelativePath.split('/')[0]
-            : ''
+          let dirName
+          if (ev.target?.files?.length)
+            dirName = new PathUtil(ev.target.files[0].webkitRelativePath)
+              .path[0]
+          if (!dirName) {
+            reject(
+              new SFError(`No webkitdirectory found, received ${ev.target}`)
+            )
+          }
           resolve(
             new ReactivityDirDecorator(
               null,
-              new LocalFallbackDirectoryEntry(
-                dirName,
-                ev.target.files,
-                true,
-                null
-              )
+              new LocalFallbackDirectoryEntry(dirName, ev.target.files, null)
             )
           )
         }
